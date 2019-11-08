7ecd3735c9f7e35822a17837633b9e54dc82b521
Kaspa-maker <kaspergutek@gmail.com>
2019-11-08 16:46:05 +0000

    Tracks page done improved drivers grid, finished f2 and f3 grids

--- src/components/drivers.js
+++ src/components/drivers.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component , Row} from 'react';
 import {Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, Button, CardActions} from 'react-mdl';
 
 class Drivers extends Component {
@@ -44,15 +44,100 @@ class Drivers extends Component {
                         <Button colored>Profile</Button>
                     </CardActions>
                 </Card>
+
+                {/*<Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(https://www.formula1.com/content/fom-website/en/drivers/sebastian-vettel/_jcr_content/image.img.640.medium.jpg/1567175200601.jpg'}}>Sebastian Vettel</CardTitle>
+                    <CardText>
+                    Sebastian Vettel (born 3 July 1987)[1] is a German racing driver who races in Formula One 
+                    for Scuderia Ferrari. He is a four-time Formula One World Champion, having won consecutive 
+                    titles in 2010–2013 with Red Bull Racing
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+            </Card>*/}
             </div>
             )
         } else if(this.state.activeTab === 1){
             return(
-                <div><h1>Hamilton</h1></div>
+            <div className="drivers-grid">
+                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(https://pbs.twimg.com/profile_images/1171794348545052672/8BYyM6DY_400x400.jpg'}}>Nyck de Vries</CardTitle>
+                    <CardText>
+                        Nyck de Vries (born 6 February 1995) is a Dutch racing driver currently competing in 
+                        Formula 2 for ART Grand Prix. He won the 2010 and 2011 Karting World Championships and 
+                        in 2014 the Formula Renault 2.0 Alps and Formula Renault 2.0 Eurocup Championships. 
+                        He was signed to the McLaren young driver programme from January 2010 until May 2019, 
+                        and Audi Sport racing academy in 2016. In 2019 he won the FIA Formula 2
+                        World Championship
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+                </Card>
+
+                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(https://www.motorsport-total.com/img/2019/190828/279039_w620_h500.jpg?ts=1566974192'}}>Nicholas Latifi</CardTitle>
+                    <CardText>
+                        Nicholas Latifi (born 29 June 1995) is a Canadian racing driver. 
+                        He formerly served as Racing Point Force India's F1 development driver, 
+                        and in 2019 he became Williams's test and reserve driver.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+                </Card>
+
+                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(https://www.thisisf1.com/wp-content/uploads/2019/08/Luca-Ghiotto-2020-F1-430x287.jpg'}}>Nicholas Latifi</CardTitle>
+                    <CardText>
+                    Luca Ghiotto (born 24 February 1995) is an Italian racing driver currently competing in 
+                    Formula 2 for UNI-Virtuosi Racing, and FIA WEC with Ginetta.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+                </Card>
+            </div>
             )
         } else if(this.state.activeTab === 2){
             return(
-                <div><h1>Hamilton</h1></div>
+                <div className="drivers-grid">
+                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(http://smpracing.tv/upload/resize_cache/iblock/f92/736_384_2/f925bbffb357466fc80f4f9980ec7ac2.JPG'}}>Robert Shwartzman</CardTitle>
+                    <CardText>
+                        Robert Mikhailovich Shwartzman (born 16 September 1999) is a Russian racing driver 
+                        currently competing in the FIA Formula 3 Championship. He is a member of the Ferrari Driver Academy. 
+                        He is the 2018 Toyota Racing Series champion and the 2019 FIA Formula 3 champion.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+                </Card>
+
+                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(https://pbs.twimg.com/profile_images/1122919050219278338/tny--5SG_400x400.jpg'}}>Marcus Armstrong</CardTitle>
+                    <CardText>
+                        Marcus Armstrong (born 29 July 2000) is a New Zealand motor racing 
+                        driver who competes in the 2019 FIA F3 Championship for Prema Powerteam.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+                </Card>
+
+                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                    <CardTitle expand style={{color: '#fff', background: 'url(https://images.financialexpress.com/2017/07/Jewan-Ashok-Twitter.jpg'}}>Jehan Daruvela</CardTitle>
+                    <CardText>
+                        Jehan Daruvala (born 1 October 1998) is an Indian racing driver, currently competing in the FIA 
+                        Formula 3 Championship with Prema Powerteam. He is a protégé of the Force India F1 team, 
+                        after being one of three winners of a 'One in a Billion hunt' organized by the team in 2011
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Profile</Button>
+                    </CardActions>
+                </Card>
+            </div>
             )
         }
     }
--- src/components/tracks.js
+++ src/components/tracks.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
- 
+import {Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, Button, CardActions} from 'react-mdl';
 
 class Tracks extends Component {
     render(){
         return(
-            <div>
-                <h1>Tracks available in the F1 season</h1>
+            <div className="category-tabs">
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">
+                            <div className="drivers-grid">
+                                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                                    <CardTitle expand style={{color: '#fff', background: 'url(https://www.austadiums.com/stadiums/photos/albert-park.jpg'}}>Melbourne Grand Prix</CardTitle>
+                                    <CardText>
+                                        The Melbourne Grand Prix Circuit is a street circuit around Albert Park Lake, only a few kilometres 
+                                        outh of central Melbourne. It is used annually as a racetrack for the Formula One Australian Grand Prix, 
+                                        Supercars Championship Melbourne 400 and associated support races. The circuit has an FIA Grade 1 licence.
+                                    </CardText>
+                                    <CardActions border>
+                                        <Button colored>Profile</Button>
+                                    </CardActions>
+                                </Card>
+
+                                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                                    <CardTitle expand style={{color: '#fff', background: 'url(https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-eu-west-1.amazonaws.com%2Fhtsi-ez-prod%2Fez%2Fimages%2F5%2F8%2F5%2F0%2F930585-1-eng-GB%2F354a4235-9da4-4b7e-afef-39422288175b.jpg?width=620&dpr=1&format=jpg&source=htsi'}}>Bahrain Grand Prix</CardTitle>
+                                    <CardText>
+                                    The Bahrain International Circuit is a motorsport venue opened in 2004 and used for drag racing, 
+                                    GP2 Series and the annual Bahrain Grand Prix. The 2004 Grand Prix was the first held in the Middle East.
+                                    <CardActions border>
+                                        <Button colored>Profile</Button>
+                                    </CardActions>
+                                    </CardText>
+                                </Card>
+
+                                <Card shadow={6} style={{width: '400px', height: '400px', margin: 'auto'}}>
+                                    <CardTitle expand style={{color: '#fff', background: 'url(https://i.pinimg.com/474x/f9/f4/95/f9f495fb26739fe14f84d3d2408da8d4--aerial-view-race-tracks.jpg'}}>Chinese Grand Prix</CardTitle>
+                                    <CardText>
+                                    The Shanghai International Circuit is a motorsport race track, situated in the Jiading District, Shanghai. 
+                                    The circuit is best known as the venue for the annual Formula 1 Chinese Grand Prix which has been hosted since 2004.
+                                    </CardText>
+                                    <CardActions border>
+                                        <Button colored>Profile</Button>
+                                    </CardActions>
+                                </Card>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
