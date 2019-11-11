59ec300b4073e24d3f4675feb2945e6d9a805b0e
Kaspa-maker <kaspergutek@gmail.com>
2019-11-11 00:32:40 +0000

    Storybook added and partly working

--- stories/0-Welcome.stories.js
+++ stories/0-Welcome.stories.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
+import About from '../src/components/about';
+import { storiesOf } from '@storybook/react';
+import Drivers from '../src/components/drivers';
+import InfoNews from '../src/components/infonews';
+import MainPage from '../src/components/mainPage';
+import Tracks from '../src/components/tracks';
 
 export default {
   title: 'Welcome',
@@ -11,3 +17,9 @@ export const toStorybook = () => <Welcome showApp={linkTo('Button')} />;
 toStorybook.story = {
   name: 'to Storybook',
 };
+
+storiesOf("F1-app/About",module).add("default",()=><About/>)
+storiesOf("F1-app/Drivers",module).add("default",()=><Drivers/>)
+storiesOf("F1-app/InfoNews",module).add("default",()=><InfoNews/>)
+storiesOf("F1-app/Main Page",module).add("default",()=><MainPage/>)
+storiesOf("F1-app/Tracks",module).add("default",()=><Tracks/>)
